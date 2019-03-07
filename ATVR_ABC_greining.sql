bf4d25c587fdff3ba49455d7e17d912a95578fd5
valdimard <valdimard@gmail.com>
2019-03-07 15:24:49 +0000

    Test.

---
+++ ATVR_ABC_greining.sql
@@ -0,0 +1,18 @@
+SELECT h.vorunumer, h.yfirflokkur, v.nafn, v.millilitrar, v.eining, sum(h.solumagn)
+FROM hreyfingar h, vorur v
+    WHERE h.vorunumer = v.vorunumer
+    AND h.yfirflokkur in ('60', '61', '62')
+GROUP BY h.vorunumer, h.yfirflokkur, v.nafn, v.millilitrar, v.eining
+    HAVING max(h.solumagn) > 0
+ORDER BY sum(h.solumagn) DESC;
+
+SELECT sum(solumagn)
+FROM hreyfingar
+WHERE yfirflokkur in ('60', '61', '62');
+
+
+
+
+
+
+
