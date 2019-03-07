37b84183b79f9288059fa9ecf5f3b5947c5e7263
valdimard <valdimard@gmail.com>
2019-03-07 19:15:49 +0000

    Merge branch 'master' of https://github.com/valdimard/crispy-winner
    # Conflicts:
    #	ATVR_ABC_greining.sql

--- ATVR_ABC_greining.sql
+++ ATVR_ABC_greining.sql
@@ -6,13 +6,36 @@ GROUP BY h.vorunumer, h.yfirflokkur, v.nafn, v.millilitrar, v.eining
     HAVING max(h.solumagn) > 0
 ORDER BY sum(h.solumagn) DESC;
 
-SELECT * FROM yfirflokkur;
 
-SELECT sum(solumagn)
-FROM hreyfingar
-WHERE yfirflokkur in ('60', '61', '62');
+SELECT h.vorunumer, v.nafn, v.millilitrar, v.eining, t."Solumagn" AS "Sala",
+    sum(t."Solumagn") OVER (ORDER BY t."Solumagn" DESC) AS "Uppsöfnuð Sala",
+    ROUND(sum(t."Solumagn") OVER (ORDER BY t."Solumagn" DESC) / sum(t."Solumagn") OVER () , 5)*100 AS "Uppsafnað hlutfall",
+    sum(t."Solumagn") OVER () AS "Heildarsala",
+    CASE
+        WHEN (sum(t."Solumagn") OVER (ORDER BY t."Solumagn" DESC) / sum(t."Solumagn") OVER ())*100 <= 80
+            THEN 'A'
+        WHEN (sum(t."Solumagn") OVER (ORDER BY t."Solumagn" DESC) / sum(t."Solumagn") OVER ())*100 <= 95
+            THEN 'B'
+        ELSE 'C'
+    END AS Class
+FROM (SELECT h2.vorunumer as "Vorunumer", sum(h2.solumagn) as "Solumagn"
+     FROM hreyfingar h2, vorur v2
+     WHERE h2.vorunumer = v2.vorunumer
+         AND h2.yfirflokkur in ('60', '61', '62')
+     GROUP BY h2.vorunumer
+    HAVING max(h2.solumagn) > 0
+ORDER BY sum(h2.solumagn) DESC) t,
+    hreyfingar h, vorur v
+    WHERE h.vorunumer = v.vorunumer
+    AND h.vorunumer = t."Vorunumer"
+    AND h.yfirflokkur in ('60', '61', '62')
+    GROUP BY t."Vorunumer", h.vorunumer, v.nafn, t."Solumagn", v.millilitrar, v.eining
+ORDER BY t."Solumagn" DESC;
 
-
-
-
-
+SELECT h2.vorunumer as "Vorunumer", sum(h2.solumagn) as "Solumagn"
+     FROM hreyfingar h2, vorur v2
+     WHERE h2.vorunumer = v2.vorunumer
+         AND h2.yfirflokkur in ('60', '61', '62')
+     GROUP BY h2.vorunumer
+    HAVING max(h2.solumagn) > 0
+ORDER BY sum(h2.solumagn) DESC
