8d8fac2822af1188888e40358ed133604324b322
valdimard <valdimard@gmail.com>
2019-05-02 14:52:46 +0000

    Merge branch 'master' of https://github.com/valdimard/crispy-winner
    # Conflicts:
    #	ATVR_ABC_greining.sql

--- ATVR_ABC_greining.sql
+++ ATVR_ABC_greining.sql
@@ -17,13 +17,54 @@ SELECT h.vorunumer, v.nafn, v.verd, v.upprunaland, v.millilitrar, v.eining, t."S
 FROM (SELECT h2.vorunumer as "Vorunumer", sum(h2.solumagn) as "Solumagn"
      FROM hreyfingar h2, vorur v2
      WHERE h2.vorunumer = v2.vorunumer
-         AND h2.yfirflokkur in ('01')
+         AND h2.yfirflokkur in ('61', '62')
      GROUP BY h2.vorunumer
     HAVING max(h2.solumagn) > 0
 ORDER BY sum(h2.solumagn) DESC) t,
     hreyfingar h, vorur v
     WHERE h.vorunumer = v.vorunumer
     AND h.vorunumer = t."Vorunumer"
-    AND h.yfirflokkur in ('01')
+    AND h.yfirflokkur in ('61', '62')
     GROUP BY t."Vorunumer", h.vorunumer, v.nafn, v.verd, v.upprunaland, t."Solumagn", v.millilitrar, v.eining, v.abc
 ORDER BY t."Solumagn" DESC;
+
+SELECT h.vorunumer, v.nafn, max(h.birgdir) as "Max birgdir", r.class, v.abc
+FROM hreyfingar h, raudvin_allt r, vorur v
+WHERE r.class = 'C'
+      AND r.abc NOT IN ('3')
+      AND r.vorunumer = h.vorunumer
+      AND h.vorunumer = v.vorunumer
+GROUP BY h.vorunumer, v.nafn, r.class, v.abc
+ORDER BY max(h.birgdir) DESC
+
+SELECT sum(h.birgdir) as "Birgdir", sum(h.solumagn) as "Solumagn", sum(h.aukning) as "Aukning", h.dagsetning
+FROM hreyfingar h, raudvin_allt r
+WHERE r.class = 'C'
+    AND r.abc NOT IN ('3')
+    AND h.vorunumer = r.vorunumer
+GROUP BY h.dagsetning
+
+SELECT h.vorunumer, v.nafn, sum(h.aukning) - sum(h.solumagn) AS "aukning-solumagn", max(h.birgdir) as "Max Birgdir", max(h.aukning) as "Max aukning", max(h.solumagn) as "Max solumagn"
+FROM hreyfingar h, raudvin_allt r, vorur v
+WHERE r.class = 'C'
+    AND r.abc NOT IN ('3')
+    AND h.vorunumer = r.vorunumer
+    AND h.vorunumer = v.vorunumer
+GROUP BY h.vorunumer, v.nafn
+ORDER BY sum(h.aukning) - sum(h.solumagn) DESC
+
+SELECT avg(h.birgdir) as "Avg birgdir", r.class, v.abc
+FROM hreyfingar h, raudvin_allt r, vorur v
+WHERE r.class = 'C'
+    AND r.vorunumer = h.vorunumer
+    AND h.vorunumer = v.vorunumer
+GROUP BY r.class, v.abc
+ORDER BY avg(h.birgdir) DESC
+
+SELECT avg(h.birgdir) as "Avg birgdir", r.class, v.abc
+FROM hreyfingar h, raudvin_allt r, vorur v
+WHERE v.abc = '3'
+    AND r.vorunumer = h.vorunumer
+    AND h.vorunumer = v.vorunumer
+GROUP BY r.class, v.abc
+ORDER BY avg(h.birgdir) DESC
